50b39b3da06c5e6fa73f6d6ad1e6e230ab71a9a0
Denis Buryachkovskiy <zekatsi5@gmail.com>
2020-11-17 20:03:07 +0200

    start working on task

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-const app = document.querySelector('#app ');
+const answer = prompt('List numbers');
+const arrOfNumbers = answer.split(',');
 
-const randomNumber = Math.floor(Math.random() * (10 - 5) + 5);
-
-let v = Math.sqrt(Math.pow(10, 3));
-
-console.log(v);
+console.log(arrOfNumbers);
