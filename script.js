e2822b206b3caefce6f2b003e6cbcc17e2831057
Denis Buryachkovskiy <zekatsi5@gmail.com>
2020-11-20 19:17:35 +0200

    lesson 3

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-const answer = prompt('List numbers');
-const arrOfNumbers = answer.split(',');
+// let answer = prompt('Are you there?');
 
-console.log(arrOfNumbers.length);
+// if (answer === 'false') {
+//   booleanAnswer = false;
+// } else {
+//   booleanAnswer = true;
+// }
+
+// let booleanAnswer = answer === 'false' ? false : true;
+
+// if (answer === 'false') {
+//   booleanAnswer = false;
+// } else {
+//   if (answer === null) {
+//     booleanAnswer = null;
+//   } else {
+//     booleanAnswer = true;
+//   }
+// }
+
+// let booleanAnswer = answer === 'false' ? false : answer === null ? null : true;
+
+// if (answer === 'false' || answer === '-1') {
+//   booleanAnswer = false;
+// }
+
+confirm('Are you sure?') && console.log('True clicked');
