c17ce6600e91e29adf4d06dedd043bec4e6a138e
Denis Buryachkovskiy <zekatsi5@gmail.com>
2020-11-27 19:12:06 +0200

    functions

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
-// const box = document.querySelector('.box');
-// box.style.marginLeft = '0px';
+// let n = +prompt('Enter a number') || 0;
+// let amount = +prompt('Amount of numbers') || 0;
 
-// const maxMargin = 200;
-// let currentMargin = 200;
+/**
+ * Will log numbers that could be devided by first argument
+ * @param {number} number Number to devide
+ * @param {number} amountOfNumbers Amount of numbers should be shown
+ */
+const calc = (number, amountOfNumbers) => {
+  let str = '';
+  for (let i = 0; i < amountOfNumbers; i++) {
+    str += `${!i ? '' : ','}${(i + 1) * number}`;
+  }
+  return str;
+};
 
-// let rand = Math.floor(Math.random() * 10);
-// let answer;
-// do {
-//   answer = prompt('Gess a number!');
-// } while (answer !== null && Number(answer) !== rand);
+// const calc = (number, amountOfNumbers) =>
+//   Array(amountOfNumbers)
+//     .fill(1)
+//     .map((_, i) => (i + 1) * number)
+//     .join(',');
 
-// console.log('Random number was', rand);
+/**
+ * Return sum of values
+ * @param {number} a First operand
+ * @param {number} b Second operand
+ */
+const sum = (a = 0, b = 0) => {
+  return a + b;
+};
 
-for (let i = 1; i < 10; i++) {
-  if (!(i % 2)) break;
-  console.log(i);
-}
+const resultOfCalc = calc(4, 10);
+
+console.log(resultOfCalc);
