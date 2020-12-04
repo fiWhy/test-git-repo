27a2168ce697ae3f0eab3bfb0cdf377d0aec6661
Denis Buryachkovskiy <zekatsi5@gmail.com>
2020-12-04 20:52:27 +0200

    instant bind

--- script.js
+++ script.js
@@ -15,9 +15,13 @@ function User(name, language, exp) {
   this.exp = exp || 0;
   this.salary = (this.exp + 1) * 100;
 
+  this.increaseSalary = (by) => {
+    this.salary += by;
+  };
+
   // this.increaseSalary = function (by) {
   //   this.salary += by;
-  // };
+  // }.bind(this);
 }
 
 const vasya = new User('Vasya', 'JavaScript');
@@ -26,11 +30,11 @@ const petya = new User('Patya', 'JavaScript');
 // petya.increaseSalary.call(vasya, 200);
 // increaseSalary.apply(vasya, [200]);
 
-const fnc = increaseSalary.bind(vasya);
+// const fnc = increaseSalary.bind(vasya);
 
 // petya.increaseSalary = vasya.increaseSalary;
-petya.increaseSalary = fnc.bind(petya);
+// petya.increaseSalary = fnc.bind(petya);
 
-petya.increaseSalary(200);
+petya.increaseSalary.call(vasya, 200);
 
 console.log(vasya, petya);
