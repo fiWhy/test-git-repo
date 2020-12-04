bd4727e64e97a5529b9fa1d57282327ba4407646
Denis Buryachkovskiy <zekatsi5@gmail.com>
2020-12-04 20:50:34 +0200

    context

--- script.js
+++ script.js
@@ -8,10 +8,6 @@ const increaseSalary = function (by) {
   this.salary += by;
 };
 
-const user = {
-  salary: 100,
-};
-
 function User(name, language, exp) {
   this.name = name;
   this.language = language;
@@ -19,21 +15,21 @@ function User(name, language, exp) {
   this.exp = exp || 0;
   this.salary = (this.exp + 1) * 100;
 
-  this.increaseSalary = (by) => {
-    this.salary += by;
-  };
+  // this.increaseSalary = function (by) {
+  //   this.salary += by;
+  // };
 }
 
 const vasya = new User('Vasya', 'JavaScript');
 const petya = new User('Patya', 'JavaScript');
 
-// increaseSalary.call(vasya, 200);
+// petya.increaseSalary.call(vasya, 200);
 // increaseSalary.apply(vasya, [200]);
 
 const fnc = increaseSalary.bind(vasya);
 
-petya.increaseSalary = vasya.increaseSalary;
-petya.increaseSalary = fnc;
+// petya.increaseSalary = vasya.increaseSalary;
+petya.increaseSalary = fnc.bind(petya);
 
 petya.increaseSalary(200);
 
