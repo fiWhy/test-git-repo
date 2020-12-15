71db60759237e9bef730f08f2096ccfc5aaf80e6
Denis Buryachkovskiy <zekatsi5@gmail.com>
2020-12-15 20:56:43 +0200

    callbacks

--- script.js
+++ script.js
@@ -1,32 +1,74 @@
-// string                   number            boolean                 {}
-// String                   Number            Boolean                 Object
-// 'string'.                (2).              true.                   {}.a = 2
-// new String('string')     new Number(2)     new Boolean(true) ->    { a: 2 }
-// own -> Object            own -> Object     own -> Object           Object
-// 'string' <- new String   2 <- new Number   true <- new Boolean     {}
-//                                                                    {a:2}.a -> 2
+const log = (data) => console.log(data);
 
-const obj = {
-  b: 2,
-  c: 3,
-  d: 4,
+function User(name, salary, language) {
+  this.name = name;
+  this.language = language;
+  this.online = true;
+  this.salary = salary;
+}
+
+User.prototype.increaseSalary = function () {
+  this.salary += 100;
 };
 
-const newArr = Array(10).fill(obj);
+User.prototype.toggleOnline = function () {
+  this.online = !this.online;
+};
 
-// const arr = [1, 2, 3];
-// arr.push(4, 5, 6, 7,  8);
+const users = [
+  {
+    name: 'Vasya',
+    salary: 1000,
+  },
+  {
+    name: 'Petya',
+    salary: 1000,
+  },
+  {
+    name: 'Grigory',
+    salary: 1000,
+  },
+];
 
-// [obj x 10]
-// [obj x 10]
+const map = (arr, cb) => {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(cb(arr[i]));
+  }
+  return newArr;
+};
 
-// (arrCopy[5]).b = '3443';
-// obj.b = '3443'
+const filter = (arr, cb) => {
+  const newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    if (cb(element)) {
+      newArray.push(element);
+    }
+  }
+  return newArray;
+};
 
-const arrCopy = newArr.slice();
+const arrOfUsers = map(users, (data) => new User(data.name, data.salary));
+const arrOfNumbers = map([1, 2, 3, 4, 'abc', 1, 3], Number);
+// const anotherArrayOfUsers = users.map(
+//   (data) => new User(data.name, data.salary)
+// );
+const anotherFilteredElements = arrOfNumbers.filter(
+  (element) => !Number.isNaN(element)
+);
+// console.log(filter(arrOfNumbers, (element) => !Number.isNaN(element)));
 
-console.log(newArr[5] === arrCopy[5]);
+console.log(anotherFilteredElements);
 
-arrCopy[5].b = 'abc';
+// map([1,2,3, '2'], Number) -> [1,2,3,2]
+// fnc([1,2,3, 'abc']) -> [1,2,3]
 
-console.log(obj.b);
+const mappedUsers = users.map(
+  (user) => new User(user.name, user.salary, 'JavaScript')
+);
+
+mappedUsers[0].increaseSalary();
+mappedUsers[2].increaseSalary();
+
+console.log(mappedUsers.filter((user) => user.salary > 1000));
