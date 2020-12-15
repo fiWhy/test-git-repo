26990f7269607dc4106a1654c7793ff56d4cc7cc
Denis Buryachkovskiy <zekatsi5@gmail.com>
2020-12-15 19:18:29 +0200

    wrapper object

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
-const recipe1 = {
-  a: 2,
-  b: 3,
-  c: 4,
+// string                   number            boolean                 {}
+// String                   Number            Boolean                 Object
+// 'string'.                (2).              true.                   {}.a = 2
+// new String('string')     new Number(2)     new Boolean(true) ->    { a: 2 }
+// own -> Object            own -> Object     own -> Object           Object
+// 'string' <- new String   2 <- new Number   true <- new Boolean     {}
+//                                                                    {a:2}.a -> 2
+
+const obj = {
+  b: 2,
+  c: 3,
+  d: 4,
 };
 
-const recipe2 = {
-  c: 4,
-  a: 2,
-  b: 3,
-};
+const newArr = Array(10).fill(obj);
 
-// [[c, 4]:0  [a, 2]:1  [b, 3]:2 ]
-// [[a, 2]:0  [b, 3]:1  [c, 4]:2 ]
+// const arr = [1, 2, 3];
+// arr.push(4, 5, 6, 7,  8);
 
-const toArray = (obj) => Object.entries(obj);
-const checkQueueAndValue = (obj1, obj2) => {
-  const obj2Array = toArray(obj2);
-  return toArray(obj1).every(
-    ([key, value], i) => obj2Array[i][0] === key && obj2Array[i][1] === value
-  );
-};
+// [obj x 10]
+// [obj x 10]
 
-console.log(checkQueueAndValue(recipe1, recipe2));
+// (arrCopy[5]).b = '3443';
+// obj.b = '3443'
+
+const arrCopy = newArr.slice();
+
+console.log(newArr[5] === arrCopy[5]);
+
+arrCopy[5].b = 'abc';
+
+console.log(obj.b);
