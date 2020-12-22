20c78e7ee9477e6a2c649ab36aa68a9202d90594
Denis Buryachkovskiy <zekatsi5@gmail.com>
2020-12-22 21:18:30 +0200

    closure

--- script.js
+++ script.js
@@ -1,50 +1,62 @@
-const arr = [1, 2, 3];
-const obj = {
-  tasks: [],
+// const Settings = (() => {
+//   let instance;
+//   const localCheck = Symbol();
+//   function Constructor(check) {
+//     if (check !== localCheck) throw new Error('Try another method');
+
+//     this.width = Math.random() * 100;
+//     this.height = Math.random() * 100;
+//   }
+
+//   Constructor.getInstance = () =>
+//     instance ? instance : (instance = new Constructor(localCheck));
+
+//   return Constructor;
+// })();
+
+// console.log(Settings.getInstance() === Settings.getInstance());
+
+// const name = 'My games rating';
+const s = 2;
+const a = () => {
+  s;
+  // debugger;
+  const b = 5;
+  return () => {
+    b;
+    s;
+    // debugger;
+    return b;
+  };
+};
+
+const res = a();
+
+console.log(res());
+console.log(res());
+console.log(res());
+
+const sum = (a, b) => a + b;
+
+console.log(sum(1, 2));
+
+const it = {
+  tasks: [1, 2, 3],
   [Symbol.iterator]() {
-    const values = [...this.tasks];
+    const copy = this.tasks.slice();
     return {
-      next() {
-        const done = !values.length;
-        const nextValue = values.shift();
+      next: () => {
+        const value = copy.shift();
+        debugger;
         return {
-          value: !done? nextValue(): undefined,
-          done,
+          value,
+          done: !value,
         };
       },
     };
   },
 };
 
-obj.tasks.push(() => 'abc');
-obj.tasks.push(() => 'asdssg');
-obj.tasks.push(() => 'asfgfgbc');
-obj.tasks.push(() => 'agsdfgfbc');
-obj.tasks.push(() => 'abdfdsfsc');
+const iterator = it[Symbol.iterator]();
 
-// for (let v of obj) {
-//   console.log(v);
-// }
-
-const objectInterator = obj[Symbol.iterator]();
-
-console.log(objectInterator.next());
-console.log(objectInterator.next());
-console.log(objectInterator.next());
-console.log(objectInterator.next());
-console.log(objectInterator.next());
-console.log(objectInterator.next());
-console.log(objectInterator.next());
-console.log(objectInterator.next());
-
-// const iterator = arr[Symbol.iterator]();
-
-// console.log(iterator.next());
-// console.log(iterator.next());
-// console.log(iterator.next());
-// console.log(iterator.next());
-// console.log(iterator.next());
-// console.log(iterator.next());
-// console.log(iterator.next());
-// console.log(iterator.next());
-// console.log(iterator.next());
+console.log(iterator.next());
