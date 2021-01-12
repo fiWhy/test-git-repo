bea549a8975de9bee5e2abc37f58d7580d0c0f8f
Denis Buryachkovskiy <zekatsi5@gmail.com>
2021-01-12 19:46:01 +0200

    homework recursion

--- script.js
+++ script.js
@@ -1,69 +1,38 @@
-const sum = (...numbers) =>
-  !numbers.length ? 0 : numbers[0] + sum(...numbers.slice(1));
-
-/**
- * <- 1 + sum([2,3,4]) // 1 + 9 ->
- * <- 2 + sum([3,4]) // 2 + 7  ->
- * <- 3 + sum([4]) // 3 + 4 ->
- * <- 4 + sum([]) // 4 + 0 ->
- * <- 0
- *
- */
-
-let lastActiveDate = new Date();
-
-window.addEventListener('mousemove', () => {
-  lastActiveDate = new Date();
-});
-
-//
-
-const timer = (cb, timeout) => {
-  let timerId = setTimeout(function tick() {
-    cb();
-    timerId = setTimeout(tick, timeout);
-  }, timeout);
-
-  return () => clearTimeout(timerId);
-};
-
-const stopTimer = timer(() => {
-  const diff = new Date() - lastActiveDate;
-
-  if (diff > 2 * 1000) {
-    console.log('AFK');
-  }
-}, 1000);
-
-const pow = (n, p) => {
-  for (let i = 1, v = n; i < p; i++) {
-    n *= v;
-  }
-  return n;
-};
-
-const recurrPow = (n, p) => {
-  if (!p) return 1;
-  return n * recurrPow(n, p - 1);
-};
-
 const obj = {
-  a: 2,
-  b: {
-    c: 4,
-    d: {
-      e: 5,
-      f: {
-        g: 6,
+  a: {
+    b: {
+      c: {
+        d: 5,
       },
     },
   },
+  b: 8,
+  c: 10,
 };
 
-const newObj = {
-  ...obj,
+const copy = (obj) => {
+  const newObj = {};
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      newObj[key] =
+        typeof obj[key] === 'object' && typeof obj[key] !== null
+          ? copy(obj[key])
+          : obj[key];
+    }
+  }
+  return newObj;
 };
 
-newObj.b.c = 6;
+const get = (value, strPath) => {
+  if (!strPath || !strPath.length || value === null || value === undefined)
+    return value;
 
-console.log(obj.b.c);
+  const path = Array.isArray(strPath) ? [...strPath] : strPath.split('.');
+  const step = path.shift();
+
+  return get(value[step], path);
+};
+
+console.log(get(obj, ['a', 'b', 'c', 'd'])); // 5;
+console.log(get(obj, 'a.b.c.f')); // undefined;
+console.log(get(obj, 'a.e.c.f')); // undefined;
