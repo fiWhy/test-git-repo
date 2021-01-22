f7fe52b4894281a2432ee1158dbb536d42b96ebd
Denis Buryachkovskiy <zekatsi5@gmail.com>
2021-01-22 20:15:27 +0200

    last updates

--- server/index.js
+++ server/index.js
@@ -4,13 +4,25 @@ const { join } = require('path');
 const usersAddr = join(__dirname, './users.json');
 const levelsAddr = join(__dirname, './levels.json');
 
-readJSON(usersAddr, (_, data) => {
-  readJSON(levelsAddr, (_, levels) => {
-    const users = data.map((user) => {
-      const level = levels.find((lvl) => lvl.value === user.level);
-      return { ...user, level };
-    });
+// readJSON(usersAddr, (_, data) => {
+//   readJSON(levelsAddr, (_, levels) => {
+//     const users = data.map((user) => {
+//       const level = levels.find((lvl) => lvl.value === user.level);
+//       return { ...user, level };
+//     });
 
-    console.log(users);
-  });
+//     console.log(users);
+//   });
+// });
+
+readJSON(usersAddr, (_, data) => {
+  const users = [...data];
+  const lastUser = users[users.length - 1];
+  const nextId = (lastUser?.id || 0) + 1;
+  const newUser = {
+    id: nextId,
+    name: `User ${nextId}`,
+    level: Math.ceil(Math.random() * 3),
+  };
+  const users = [...users, newUser];
 });
--- server/utils/index.js
+++ server/utils/index.js
@@ -1,8 +1,11 @@
-const { readFile } = require('fs');
+const { readFile, writeFile } = require('fs');
 
 const readJSON = (addr, cb) =>
   readFile(addr, (err, data) => cb(err, JSON.parse(data.toString())));
 
+const writeJSON = (addr, data, cb) => {};
+
 module.exports = {
   readJSON,
+  writeJSON,
 };
