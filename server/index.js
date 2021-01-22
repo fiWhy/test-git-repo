0f9b9063e77ca1523f80111c3bc8a5bdf6ac2259
Denis Buryachkovskiy <zekatsi5@gmail.com>
2021-01-22 21:35:29 +0200

    prepared server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
-const { readJSON } = require('./utils');
+const { readJSON, writeJSON } = require('./utils');
+const { readFile } = require('fs');
+const { resolve } = require('path');
+const http = require('http');
 const { join } = require('path');
 
 const usersAddr = join(__dirname, './users.json');
@@ -15,14 +18,84 @@ const levelsAddr = join(__dirname, './levels.json');
 //   });
 // });
 
-readJSON(usersAddr, (_, data) => {
-  const users = [...data];
-  const lastUser = users[users.length - 1];
-  const nextId = (lastUser?.id || 0) + 1;
-  const newUser = {
-    id: nextId,
-    name: `User ${nextId}`,
-    level: Math.ceil(Math.random() * 3),
-  };
-  const users = [...users, newUser];
+// readJSON(usersAddr, (_, data) => {
+//   const users = [...data];
+//   const lastUser = users[users.length - 1];
+//   const nextId = ((lastUser && lastUser.id) || 0) + 1;
+//   const newUser = {
+//     id: nextId,
+//     name: `User ${nextId}`,
+//     level: Math.ceil(Math.random() * 3),
+//   };
+
+//   const updatedUsers = [...users, newUser];
+
+//   writeJSON(usersAddr, updatedUsers, () => {
+//     console.log('Data saved!');
+//   });
+// });
+
+/**
+ * GET http://localhost:3000/?query=abc
+ * POST http://localhost:3000
+ */
+
+const get = (req, res) => {
+  res.writeHead(200, {
+    ['Content-Type']: 'application/json',
+  });
+
+  readJSON(usersAddr, (_, data) => {
+    res.end(JSON.stringify(data));
+  });
+};
+
+const add = (req, res, body) => {
+  res.writeHead(200, {
+    ['Content-Type']: 'application/json',
+  });
+  readJSON(usersAddr, (_, data) => {
+    const lastUser = data[data.length - 1];
+    const newData = [
+      ...data,
+      {
+        ...body,
+        id: ((lastUser && lastUser.id) || 0) + 1,
+      },
+    ];
+    writeJSON(usersAddr, newData, () => {
+      res.end(JSON.stringify(newData));
+    });
+  });
+};
+
+const route = (req, res, data) => {
+  const parsedUrl = req.url.split('/').filter((part) => part);
+
+  switch (parsedUrl[0]) {
+    case 'get':
+      get(req, res);
+      break;
+    case 'add':
+      add(req, res, data);
+      break;
+    default:
+      res.end('Hi there!');
+  }
+};
+
+const server = http.createServer((req, res) => {
+  let body = [];
+  req
+    .on('data', (chunk) => {
+      body.push(chunk);
+    })
+    .on('end', () => {
+      body = Buffer.concat(body).toString();
+      route(req, res, body ? JSON.parse(body) : undefined);
+    });
+});
+
+server.listen(8080, () => {
+  console.log('You can reach the server here http://localhost:8080');
 });
--- server/utils/index.js
+++ server/utils/index.js
@@ -3,7 +3,8 @@ const { readFile, writeFile } = require('fs');
 const readJSON = (addr, cb) =>
   readFile(addr, (err, data) => cb(err, JSON.parse(data.toString())));
 
-const writeJSON = (addr, data, cb) => {};
+const writeJSON = (addr, data, cb) =>
+  writeFile(addr, JSON.stringify(data), (err) => cb(err));
 
 module.exports = {
   readJSON,
