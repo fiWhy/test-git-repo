a61d57bea570aa51534d774515f05120052ec37c
Denis Buryachkovskiy <zekatsi5@gmail.com>
2021-01-26 20:42:58 +0200

    add express

--- server/index.js
+++ server/index.js
@@ -1,59 +1,24 @@
 const { readJSON, writeJSON } = require('./utils');
-const { readFile } = require('fs');
-const { resolve } = require('path');
-const http = require('http');
 const { join } = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
 
 const usersAddr = join(__dirname, './users.json');
 const levelsAddr = join(__dirname, './levels.json');
 
-// readJSON(usersAddr, (_, data) => {
-//   readJSON(levelsAddr, (_, levels) => {
-//     const users = data.map((user) => {
-//       const level = levels.find((lvl) => lvl.value === user.level);
-//       return { ...user, level };
-//     });
+app.use(express.json());
+app.use(bodyParser.json());
 
-//     console.log(users);
-//   });
-// });
-
-// readJSON(usersAddr, (_, data) => {
-//   const users = [...data];
-//   const lastUser = users[users.length - 1];
-//   const nextId = ((lastUser && lastUser.id) || 0) + 1;
-//   const newUser = {
-//     id: nextId,
-//     name: `User ${nextId}`,
-//     level: Math.ceil(Math.random() * 3),
-//   };
-
-//   const updatedUsers = [...users, newUser];
-
-//   writeJSON(usersAddr, updatedUsers, () => {
-//     console.log('Data saved!');
-//   });
-// });
-
-/**
- * GET http://localhost:3000/?query=abc
- * POST http://localhost:3000
- */
-
-const get = (req, res) => {
-  res.writeHead(200, {
-    ['Content-Type']: 'application/json',
-  });
-
+app.get('/get', (req, res) => {
   readJSON(usersAddr, (_, data) => {
-    res.end(JSON.stringify(data));
+    res.send(data);
   });
-};
+});
 
-const add = (req, res, body) => {
-  res.writeHead(200, {
-    ['Content-Type']: 'application/json',
-  });
+app.post('/add', ({ body }, res) => {
   readJSON(usersAddr, (_, data) => {
     const lastUser = data[data.length - 1];
     const newData = [
@@ -64,38 +29,15 @@ const add = (req, res, body) => {
       },
     ];
     writeJSON(usersAddr, newData, () => {
-      res.end(JSON.stringify(newData));
+      res.send(newData);
     });
   });
-};
-
-const route = (req, res, data) => {
-  const parsedUrl = req.url.split('/').filter((part) => part);
-
-  switch (parsedUrl[0]) {
-    case 'get':
-      get(req, res);
-      break;
-    case 'add':
-      add(req, res, data);
-      break;
-    default:
-      res.end('Hi there!');
-  }
-};
-
-const server = http.createServer((req, res) => {
-  let body = [];
-  req
-    .on('data', (chunk) => {
-      body.push(chunk);
-    })
-    .on('end', () => {
-      body = Buffer.concat(body).toString();
-      route(req, res, body ? JSON.parse(body) : undefined);
-    });
 });
 
-server.listen(8080, () => {
-  console.log('You can reach the server here http://localhost:8080');
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+app.listen(port, () => {
+  console.log(`Listening http://localhost:${port}`);
 });
