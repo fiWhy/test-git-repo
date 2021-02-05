f00a0a728bac5e365ac5564e7193ad9ffc96b092
Denis Buryachkovskiy <zekatsi5@gmail.com>
2021-02-05 20:07:40 +0200

    dom start

--- client/src/script.js
+++ client/src/script.js
@@ -1,21 +1,5 @@
-import { last, random } from 'lodash';
+import { logNestedElements } from './utils';
 
-const getUsers = () => fetch('http://localhost:3000/get');
-const addUser = (body) =>
-  fetch('http://localhost:3000/add', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(body),
-  });
-const getJsonFromResponse = (response) => response.json();
+const app = document.getElementById('app');
 
-getUsers().then(getJsonFromResponse).then(last);
-//   .then((user) =>
-//     addUser({
-//       name: user.name + 1,
-//       level: random(0, 3),
-//     })
-//   )
-//   .then(console.log);
+logNestedElements(app);
--- client/src/utils.js
+++ client/src/utils.js
@@ -1,5 +1,11 @@
-export const sum = (a, b) => a + b;
-
-const a = () => {};
-
-export default a;
+export const logNestedElements = (root) => {
+  root.childNodes.forEach((node) => {
+    if (node.childNodes && node.childNodes.length) {
+      console.group(`Element: ${node.tagName} - ${node.nodeType}`);
+      logNestedElements(node);
+      console.groupEnd();
+    } else {
+        console.log(`${node.nodeName}: ${node.nodeValue} - ${node.nodeType}`);
+    }
+  });
+};
