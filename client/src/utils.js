60a47ed8a2279998c16ad7494874a2ab32d4bf9a
Denis Buryachkovskiy <zekatsi5@gmail.com>
2021-02-09 21:23:39 +0200

    add listeners

--- client/src/utils.js
+++ client/src/utils.js
@@ -25,22 +25,18 @@ export const appendChildren = (el, children = []) => {
   return newElement;
 };
 
-export const addListeners = (el, listeners = {}) => {
-  const newElement = el.cloneNode();
-  Object.entries(listeners).forEach(([listener, callback]) => {
-    newElement.addEventListener(listener, callback);
-  });
-  return newElement;
-};
-
 export const element = (
   tagName,
   { children = [], classNames = [], listeners = {}, ...additionalProps }
 ) =>
   chain(document.createElement(tagName))
     .thru((el) => addClasses(el, classNames))
-    .thru((el) => addListeners(el, listeners))
     .thru((el) => appendChildren(el, children))
+    .tap((el) => {
+      Object.entries(listeners).forEach(([listener, callback]) => {
+        el.addEventListener(listener, callback);
+      });
+    })
     .value();
 
 export const div = (props) => element('div', props);
