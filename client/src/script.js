fe674a9c997260c677282a4b6d15a603c508418c
Denis Buryachkovskiy <zekatsi5@gmail.com>
2021-02-09 20:41:54 +0200

    add listeners structure

--- client/src/script.js
+++ client/src/script.js
@@ -2,21 +2,48 @@ import { div, get } from './utils';
 
 get().then((data) => {
   const app = document.getElementById('app');
-  data.map((user) => {
-    const card = div({
-      classNames: ['card'],
-      children: [
-        div({
-          classNames: ['card__header'],
-          children: [user.name],
-        }),
-        div({
-          classNames: ['card__body'],
-          children: [user.level],
-        }),
-      ],
-    });
+  let currentSlide = 0;
+  const wrapper = div({
+    classNames: ['gallery'],
+    children: [
+      div({
+        classNames: ['gellery__prev__wrapper'],
+        listeners: {
+          click: () => (currentSlide = !currentSlide ? 0 : currentSlide - 1),
+        },
+        children: ['<'],
+      }),
+      div({
+        classNames: ['gellery__next__wrapper'],
+        listeners: {
+          click: () =>
+            (currentSlide =
+              currentSlide === data.length ? data.length : currentSlide + 1),
+        },
+        children: ['>'],
+      }),
+      div({
+        classNames: ['gallery__cards__wrapper'],
+        children: data.map((user) => {
+          const card = div({
+            classNames: ['card'],
+            children: [
+              div({
+                classNames: ['card__header'],
+                children: [user.name],
+              }),
+              div({
+                classNames: ['card__body'],
+                children: [user.level],
+              }),
+            ],
+          });
 
-    app.append(card);
+          return card;
+        }),
+      }),
+    ],
   });
+
+  app.append(wrapper);
 });
